235322f8e201071ffe8aa6ddff874620c0844518
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-04 21:48:16 +0100

    WeekendCupcakes.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,29 @@
+player1_decision =int(input("Enter rock, paper or scissors: "))
+player2_decision =int(input("Enter rock, paper or scissors: "))
+
+player1_decision =player1_decision.lower()
+player1_decision =player1_decision.lower()
+
+if (player1_decision == player2_decision):
+    print("Tie")
+elif (player1_decision == rock and player2_decision==scissors):
+    print("Player1 Wins")
+elif (player1_decision == scissors and player2_decision==paper):
+    print("Player1 Wins")
+elif (player1_decision == paper and player2_decision==rock):
+    print("Player1 Wins")
+
+
+elif (player2_decision== rock and player1_decision==scissors):
+    print("Player2 Wins")
+elif (player2_decision== scissor and player1_decision==paper):
+    print("Player2 Wins")
+elif (player2_decision== paper and player1_decision==rock):
+    print("Player2 Wins")
+else:
+    print("Invalid Input")           
+
+
+
+#.title
+#.capitalize
